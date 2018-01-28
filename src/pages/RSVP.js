e09b5e3c0be2e508bf170dc932e538bf43d6b3f5
Kim Strauch <kim.strauch@airbnb.com>
2018-01-27 21:23:18 -0800

    add some intelligence around users who have already rsvp'd

--- src/pages/RSVP.js
+++ src/pages/RSVP.js
@@ -7,10 +7,26 @@ const encode = (data) => {
       .join("&");
 }
 
+const setRSVP = (info) => {
+  try {
+    window.localStorage.setItem('rsvp', true);
+  } catch(e) {
+    // do nothing for now
+  }
+}
+
+const getRSVP = () => {
+  try {
+    return window.localStorage.getItem('rsvp');
+  } catch(e) {
+    return false;
+  }
+}
+
 class RSVP extends Component {
   constructor(props) {
     super(props);
-    this.state = { name: '', email: '', partySize: '', comments:  ''};
+    this.state = { name: '', email: '', attendance: '', comments:  '', hasRSVPd: getRSVP()};
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -20,26 +36,29 @@ class RSVP extends Component {
     });
   }
   handleSubmit(e) {
+    const {name, email, attendance, comments } = this.state;
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "rsvp", ...this.state })
+      body: encode({ "form-name": "rsvp", name, email, comments, attendance })
     })
       .then(() => {
-        alert("Your response has been recorded!");
+        setRSVP({name, email, attendance, comments});
+        alert("Thanks for letting us know! Use this form again or email us if anything changes.");
         this.setState({
           name: '',
           email: '',
           comments: '',
           attendance: '',
         });
+
       })
       .catch(error => alert(error));
 
     e.preventDefault();
   };
   render() {
-    const { name, email, comments, attendance } = this.state;
+    const { name, email, comments, attendance, hasRSVPd } = this.state;
     return (
       <div className="container">
         <img src={rock} alt="Kim and Phil on a rock in Mexico City" width="100%" height="auto" />
@@ -47,8 +66,11 @@ class RSVP extends Component {
           We'll be sending out formal invitations as well. If you already know whether you can make it, it'd rock if you could RSVP here so we can get a rough headcount.
         </p>
         <p>
-          Please RSVP once for each guest invited.
+          Please RSVP for each guest invited.
         </p>
+        { hasRSVPd && (
+          <p className='rsvp-success'>Thank you for RSVPing. Please use this form again if you'd like to update your response.</p>
+        )}
         <form className="rsvp-form" onSubmit={this.handleSubmit}>
           <input type="hidden" name="form-name" value="rsvp" />
           <div className="row">
@@ -70,7 +92,7 @@ class RSVP extends Component {
               </select>
             </div>
           </div>
-          <div classNam="row">
+          <div className="row">
             <div className="twelve columns">
               <label htmlFor="comments">Anything you'd like us to know, or any questions you have?</label>
               <textarea className="u-full-width" name="comments" value={comments} onChange={this.handleChange}>
